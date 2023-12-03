ea0131aeb7707720da5103864ecbdd4e757823fd
ECecillo <enzocecillon@MBP-de-Enzo.lan>
2023-12-03 11:40:57 +0100

    chore: add commented import in main to uncomment when defined

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import "ececillo/advent-of-code-2023/day1"
 
+// Uncomment to run other days
+// import "ececillo/advent-of-code-2023/day2"
+// import "ececillo/advent-of-code-2023/day3"
+
 func main() {
 	day1.PrintHello()
 }
